6bbd4d034ea5d4fded47684e16eff3a43ffb21c8
Danudet Boonyakamol <danudet@gmail.com>
2012-04-16 14:19:12 -0700

    im a boss

--- EclipseProject/gen/weatherOracle/app/R.java
+++ EclipseProject/gen/weatherOracle/app/R.java
@@ -13,8 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050001;
+        public static final int widget40=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int addnotification=0x7f030000;
+        public static final int editnotification=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int secretimfeelingluckyscreen=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
