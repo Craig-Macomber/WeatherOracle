4b5f976e281d5db77e37d8e8bf886fbfd223b9fa
Craig <Craigm@98115.net>
2012-04-19 11:23:29 -0700

    add more UML stuff

--- EclipseProject/src/weatherOracle/filter/Rule.java
+++ EclipseProject/src/weatherOracle/filter/Rule.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import weatherOracle.forecastData.ForecastData;
 
-abstract interface Rule extends Serializable{
+interface Rule extends Serializable{
 	public abstract Boolean apply(ForecastData data);
 	public abstract void showUI(); // TODO : Placeholder for showing UI, needs some paramaters
 }
