c647bffda9d63cfafec604f50c13483a69395cdd
Craig <Craigm@98115.net>
2012-04-27 12:46:57 -0700

    add minimal XML reading+parsing

--- EclipseProject/src/weatherOracle/forecastData/ForecastRequirements.java
+++ EclipseProject/src/weatherOracle/forecastData/ForecastRequirements.java
@@ -1,9 +1,20 @@
 package weatherOracle.forecastData;
 
+import java.util.Collections;
+import java.util.Map;
+
+import weatherOracle.app.Location;
 import weatherOracle.app.TimeRange;
 
 
 public class ForecastRequirements {
+	Map<Location,TimeRange> data;
+	public void addTimeRange(Location loc, TimeRange t){
+		//TODO
+	}
+	public Map<Location,TimeRange> getData(){
+		return Collections.unmodifiableMap(data);
+	}
 	protected TimeRange getTimeRange(){
 		// TODO
 		return null;
--- EclipseProject/src/weatherOracle/reader/NoaaReader.java
+++ EclipseProject/src/weatherOracle/reader/NoaaReader.java
@@ -1,16 +1,76 @@
 package weatherOracle.reader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import weatherOracle.forecastData.ForecastRequirements;
 import weatherOracle.forecastData.ForecastDataRange;
 import weatherOracle.app.Location;
 
-public class NoaaReader implements Reader{
-	public Map<Location,ForecastDataRange> getData(ForecastRequirements r){
-		// TODO
-		
-		return null;
-		
+public class NoaaReader implements Reader {
+	public Map<Location, ForecastDataRange> getData(ForecastRequirements r) {
+		Map<Location, ForecastDataRange> m = new HashMap<Location, ForecastDataRange>();
+
+		// http://forecast.weather.gov/MapClick.php?lat=47.66076&lon=-122.29508&FcstType=digitalDWML
+		for (Location loc : r.getData().keySet()) {
+			URL url;
+			try {
+				url = new URL("http://forecast.weather.gov/MapClick.php?lat="
+						+ loc.lat + "&lon=" + loc.lon + "&FcstType=digitalDWML");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			InputStream s;
+			try {
+				s = url.openConnection().getInputStream();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+			// Using factory get an instance of document builder
+			DocumentBuilder db;
+			try {
+				db = dbf.newDocumentBuilder();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+
+			// parse using builder to get DOM representation of the XML file
+			Document dom;
+			try {
+				dom = db.parse(s);
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+
+			ForecastDataRange d = new ForecastDataRange(r);
+			m.put(loc, d);
+		}
+		return m;
 	}
 }
