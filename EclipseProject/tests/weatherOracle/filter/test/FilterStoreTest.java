46c6b1aa2000eaeef4252ffa4c40258797c59184
Milan Justel <milan.justel@gmail.com>
2012-05-06 00:44:18 -0700

    added filterStore test

--- EclipseProject/tests/weatherOracle/filter/test/FilterStoreTest.java
+++ EclipseProject/tests/weatherOracle/filter/test/FilterStoreTest.java
@@ -9,15 +9,13 @@ import android.util.Pair;
 
 public class FilterStoreTest extends AndroidTestCase {
 	public void testFilterStore() {
-		//fail("not implemented");
-		
-		// Get with no filters inside
+		// Try getting filters when there are none in there yet
 		Pair<List<Filter>, Integer> returnFilters = FilterStore.getFilters();
 		
 		assertEquals(returnFilters.second.intValue(), 0);
 		assertTrue(returnFilters.first.isEmpty());
 		
-		// Put in some filters and get again
+		// Put in some filters and get the filters again
 		List<Filter> filters = new LinkedList<Filter>();
 		Filter f = new Filter();
 		f.addRule(new ConditionRule());
@@ -30,7 +28,6 @@ public class FilterStoreTest extends AndroidTestCase {
 		assertEquals(returnFilters.second.intValue(), 1);
 		
 		boolean pairMatch = true;
-		
 		for (int i = 0; i < returnFilters.first.size(); i++) {
 			pairMatch = pairMatch && returnFilters.first.get(i).equals(filters.get(i));
 			
