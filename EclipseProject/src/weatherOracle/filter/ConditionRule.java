c24786d8eaf1f1a078dd87c0009daf2084b58e03
Craig <Craigm@98115.net>
2012-05-08 15:07:24 -0700

    fix serialVersionUID in ConditionRule

--- EclipseProject/src/weatherOracle/filter/ConditionRule.java
+++ EclipseProject/src/weatherOracle/filter/ConditionRule.java
@@ -3,13 +3,17 @@ package weatherOracle.filter;
 import weatherOracle.forecastData.ForecastData;
 
 public class ConditionRule implements Rule {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2695287363381416690L;
+
 	// NEED SOME FIELDS HERE
 	
 	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
 
 	public ConditionRule() {
 		
