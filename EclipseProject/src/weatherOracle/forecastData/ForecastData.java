b690b4455700a0d086e94fbb724c7279cc8c691c
Craig <Craigm@98115.net>
2012-05-08 14:14:35 -0700

    add all fields to ForecastData

--- EclipseProject/src/weatherOracle/forecastData/ForecastData.java
+++ EclipseProject/src/weatherOracle/forecastData/ForecastData.java
@@ -10,7 +10,28 @@ public class ForecastData {
 	// TODO : include everything, verify units, add constructor, add accessors
 	private double windSpeed; // in m/s
 	private double windDirection; // in degrees from X
-	private double temperature; // in degrees C  
+	private double temperature_hourly; // in degrees F
+	private double temperature_dewPoint; // in degrees F
+	private double probabilityOfPrecipitation; // percent
+	private double windSpeed_sustained; // mph?
+	private double windSpeed_gust; // mph?
+	private double windSpeed_direction; // in degrees true
+	private double cloudAmount; // in percent
+	private double humidity_relative; // in percent
+	private double hourly_qpf; // inches 
+	
+	private enum Rain {
+		NONE, chance
+	}
+	
+	private enum Thunder {
+		NONE
+	}
+	
+	private Rain rain;
+	private Thunder thunder;
+	
+	// Time
 	private int hour;
 	private Date day;
 }
