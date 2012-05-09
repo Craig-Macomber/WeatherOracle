ced20aa46f0bb12f6c448b5cdf99b722a97243cd
Milan Justel <milan.justel@gmail.com>
2012-05-08 17:01:53 -0700

    modified ConditionRule. This blimp is real. IGNORE ME

--- EclipseProject/src/weatherOracle/filter/ConditionRule.java
+++ EclipseProject/src/weatherOracle/filter/ConditionRule.java
@@ -3,30 +3,29 @@ package weatherOracle.filter;
 import weatherOracle.forecastData.ForecastData;
 
 public class ConditionRule implements Rule {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2695287363381416690L;
+	
+	// All possible conditions
+	public static String[] conditions = new String[]{"TEMPERATURE", "DEWPOINT", "HEAT_INDEX", "WIND", "CLOUD_COVER",
+													"PERCIPITATION_PERCENT", "HUMIDITIY", "THUNDER", "RAIN"};
 
 	// NEED SOME FIELDS HERE
+	private String condition;
+	private int min;
+	private int max;
 	
-	
-	/**
-	 * 
-	 */
-
-	public ConditionRule() {
-		
+	public ConditionRule(String condition, int min, int max) {
+		if (condition == null || min > max) {
+			throw new IllegalArgumentException("UI you suck!");
+		}
+		this.condition = condition;
+		this.min = min;
+		this.max = max;
 	}
 
 	public Boolean apply(ForecastData data) {
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public void showUI() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
