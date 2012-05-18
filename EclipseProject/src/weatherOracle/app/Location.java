ffb6213ce55b70cbffd97e60d8d3ba383786594c
Michael Fain <micfain@gmail.com>
2012-05-18 00:38:34 -0700

    JavaDoc'd Location

--- EclipseProject/src/weatherOracle/app/Location.java
+++ EclipseProject/src/weatherOracle/app/Location.java
@@ -15,19 +15,36 @@ public class Location implements Serializable{
 	public double lat;
 	public double lon;
 
+	/**
+	 * 
+	 * @param lat=latitude of new Location
+	 * @param lon=longitude of new Location
+	 * Constructor creates a new Location object from a specified lat/long location
+	 */
 	public Location(double lat, double lon) {
 		this.lat = lat;
 		this.lon = lon;
 	}
 
+	/**
+	 * 
+	 * @param loc=android location structure to create a Location object from
+	 * Constructor creates a new Location object from an android location
+	 */
 	public Location(android.location.Location loc) {
 		this(loc.getLatitude(), loc.getLongitude());
 	}
 
+	/**
+	 * @return a hash of the object to assist in equals() calls
+	 */
 	public int hashCode() {
 		return new Double(lat).hashCode() ^ new Double(lon).hashCode();
 	}
 
+	/**
+	 * @return true if this==aThat, false otherwise
+	 */
 	public boolean equals(Object aThat) {
 		if (this == aThat)
 			return true;
@@ -39,10 +56,19 @@ public class Location implements Serializable{
 	
 	private static android.location.Location myCurrentLocation;
 	
+	/**
+	 * 
+	 * @return the user's current Lat/Long location as recorded by the phone
+	 */
 	public static Location getCurrentLocation()
 	{
 		return new Location(myCurrentLocation.getLatitude(),myCurrentLocation.getLongitude());
 	}
+	/**
+	 * 
+	 * @param loc=new location to assign as the phone's current location
+	 * @modify the user's current location
+	 */
 	public static void setCurrentLocation(android.location.Location loc)
 	{
 		myCurrentLocation=loc;
