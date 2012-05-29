18cb82607a63be89ab0228f31a9eb719501de1fb
Milan Justel <milan.justel@gmail.com>
2012-05-29 16:01:59 -0700

    fixed aesthetic issue

--- EclipseProject/src/weatherOracle/activity/ConditionRuleViewerActivity.java
+++ EclipseProject/src/weatherOracle/activity/ConditionRuleViewerActivity.java
@@ -64,7 +64,7 @@ public class ConditionRuleViewerActivity extends Activity {
     		int min = conditionList.get(i).getMinMax().first;
     		int max = conditionList.get(i).getMinMax().second;
     		String range;
-    		range = min + ConditionRule.getUnits(conditionList.get(i).getCondition()) + " - " + max + ConditionRule.getUnits(conditionList.get(i).getCondition());
+    		range = min + ConditionRule.getUnits(conditionList.get(i).getCondition()) + " to " + max + ConditionRule.getUnits(conditionList.get(i).getCondition());
     		
     		textview.setText(conditionList.get(i).getCondition() + ":\n " + range);
     		textview.setTextSize(2,15);
